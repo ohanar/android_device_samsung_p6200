70f5791294a07c9c5adefcbaf0c0cc526104d6c4
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-08-15 18:45:56 -0700

    cleanup/reorganize

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/p6200_base.mk
+    $(LOCAL_DIR)/device.mk
--- device.mk
+++ device.mk
@@ -16,18 +16,21 @@
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/samsung/smdk4210-tab/smdk4210-tab_base.mk)
-$(call inherit-product-if-exists, vendor/samsung/smdk4210-tab/smdk4210-tab_base.mk)
-$(call inherit-product-if-exists, vendor/samsung/p6200/p6200_base.mk)
+$(call inherit-product, device/samsung/smdk4210-tab/device.mk)
+$(call inherit-product-if-exists, vendor/samsung/smdk4210-tab/vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/p6200/vendor.mk)
 
 # the Tab 7+ has a large mpdi screen
 PRODUCT_AAPT_CONFIG := large mdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 PRODUCT_LOCALES += mdpi
 
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=160
-
 # GSM
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libsec-ril.so \
+    rild.libargs=-d /dev/ttyS0 \
+    ro.telephony.ril_class=Smdk4210RIL
