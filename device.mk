103e06777bda27c51d169b24fed60e44d72186a0
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-08-18 15:44:32 -0700

    move resources to common repo

--- device.mk
+++ device.mk
@@ -20,11 +20,6 @@ $(call inherit-product, device/samsung/smdk4210-tab/device.mk)
 $(call inherit-product-if-exists, vendor/samsung/smdk4210-tab/vendor.mk)
 $(call inherit-product-if-exists, vendor/samsung/p6200/vendor.mk)
 
-# the Tab 7+ has a large mpdi screen
-PRODUCT_AAPT_CONFIG := large mdpi
-PRODUCT_AAPT_PREF_CONFIG := mdpi
-PRODUCT_LOCALES += mdpi
-
 # GSM
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
